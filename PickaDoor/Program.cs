787d5c5f21ea3667b7e395fc4a0b4c38924983e3
Bo Browning <Bo.Browning@education.nsw.gov.au>
2017-10-18 10:33:46 +1100

    we completed PICK A DOR.

---
+++ PickaDoor/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PickaDoor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pick a door");
+            string choice = Console.ReadLine();
+
+            if (choice == "1")
+            {
+                Console.WriteLine("Behind door number 1 is tex and coen puttin daddy ronin on the spit");
+            }
+            if (choice == "2")
+            {
+                Console.WriteLine("Behind door number 2 is txai slippin da disk into a fcked up giraff ");
+            }
+            if (choice == "3")
+            {
+                Console.WriteLine("Behind door number 3 is daddy ronin slippin da disk into a naked buffalo");
+            }
+            Console.ReadKey();
+        }
+    }
+        
+}
