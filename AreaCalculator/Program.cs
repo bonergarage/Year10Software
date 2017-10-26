b437b45972fa0adfd6bd57443a2fb1dd3cd86c38
Bo Browning <Bo.Browning@education.nsw.gov.au>
2017-10-26 12:39:02 +1100

    started working on area calculator.

---
+++ AreaCalculator/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string choice = "";
+            while (choice != "4") 
+            {
+                // Menu
+                Console.Clear();
+                Console.WriteLine("chose n option");
+                Console.Wrieline("1. Area of Rectangle");
+                Console.WriteLine("2.Area of Triangle");
+                Console.Wrieline("3. Area of Circle");
+                Console.WriteLine("4. Exit");
+                choice = Console.ReadLine();
+
+
+                if(choice == "1")
+                {
+                    // Rectangle
+                }
+                else if(choice == "2")
+                {
+                    // Triangle
+                }
+                else if(choice == "3")
+                {
+                    // Circle
+                }
+
+
+                // Wait
+                Console.WriteLine();
+                Console.Wrieline("Press Any Key To Continue....")
+                Console.ReadKey();
+
+
+
+
+            }
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
