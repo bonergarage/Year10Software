47eedc73eb18839dbe64f2084b27caed0229f553
Bo Browning <Bo.Browning@education.nsw.gov.au>
2017-11-08 10:41:38 +1100

    started insut generator.

---
+++ InsultGenerator/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsultGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ask for your name
+            string name = GetName();
+
+            // Generate a random insult
+            GenerateInsult(name);
+        }
+        private static string GetName()
+        {
+
+        }
+        private static void GenerateInsult(string name)
+        {
+            Console.WriteLine(name + ", you are " + GetAdjective() + GetCompoundAdjective() + GetObject());
+        }
+    }
+}       
