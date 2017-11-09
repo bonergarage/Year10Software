f2d72fd08ebf7e63d81491d9bb48c344346ae381
Bo Browning <Bo.Browning@education.nsw.gov.au>
2017-11-09 12:39:56 +1100

    finished insult genertor.

--- InsultGenerator/Program.cs
+++ InsultGenerator/Program.cs
@@ -18,11 +18,30 @@ namespace InsultGenerator
         }
         private static string GetName()
         {
-
+            Console.WriteLine("Welcome to your Insult Generator. What is your name ?");
+            return Console.ReadLine();
         }
         private static void GenerateInsult(string name)
         {
             Console.WriteLine(name + ", you are " + GetAdjective() + GetCompoundAdjective() + GetObject());
+            menu(name);
         }
+        private static void menu(string name)
+        {
+
+        }
+        private static string GetAdjective()
+        {
+            string[] Adjectives = { "small nut", "a smelly cunt", "nigga-faggot", "tiny penis", "jigantic pussy", "four-eyes looking mothafcker" };
+        }
+        private static string GetCompoundAdjective()
+        {
+            string[] CompoundAdjectives = { "trump-loving", "cum slurpin", "slut shaming", "woman bashing", "transgender-loving" };
+        }
+        private static void GetObject()
+        {
+            string[] Objects = { "thot", "rat", "fuckwit", "asian", "CHINAMAN"};
+        }
+
     }
 }       
